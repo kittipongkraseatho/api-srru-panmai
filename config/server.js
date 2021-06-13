03c754ca801b7fe3748632e079beb496b4cc4969
kittipongkraseatho <kittipong.kr.57@ubu.ac.th>
2021-06-13 22:25:24 +0700

    setup

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac32aa2f390bc19b26246126f1dfc400'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '08c3e86a-fec8-42cc-b42b-e48e6b42eedc'
+};
