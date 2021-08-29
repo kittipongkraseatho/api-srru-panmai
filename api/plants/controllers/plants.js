237e09ebd2dc53da176da2cff725ac4485e8ec46
kittipongkraseatho <kittipong.kr.57@ubu.ac.th>
2021-08-30 01:11:27 +0700

    update กราฟ

--- api/plants/controllers/plants.js
+++ api/plants/controllers/plants.js
@@ -5,4 +5,40 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async dashboard(ctx) {
+    let s_date1 = ctx.request.body.s_date1;
+    let e_date1 = ctx.request.body.e_date1;
+
+    let s_date2 = ctx.request.body.s_date2;
+    let e_date2 = ctx.request.body.e_date2;
+
+    let s_date3 = ctx.request.body.s_date3;
+    let e_date3 = ctx.request.body.e_date3;
+
+    let s_date4 = ctx.request.body.s_date4;
+    let e_date4 = ctx.request.body.e_date4;
+
+    let month1 =  strapi.query('plants').count({
+      created_at_gte: s_date1,
+      created_at_lte: e_date1,
+    });
+
+    let month2 =  strapi.query('plants').count({
+      created_at_gte: s_date2,
+      created_at_lte: e_date2,
+    });
+    let month3 =  strapi.query('plants').count({
+      created_at_gte: s_date3,
+      created_at_lte: e_date3,
+    });
+    let month4 =  strapi.query('plants').count({
+      created_at_gte: s_date4,
+      created_at_lte: e_date4,
+    });
+    let value = null;
+    let values = await Promise.all([month4, month3, month2, month1]);
+
+      return values
+  }
+};
