859a1c5309a7f37583e9023a20eec86b88952988
kittipongkraseatho <kittipong.kr.57@ubu.ac.th>
2021-09-18 23:35:38 +0700

    ทำ cache ไฟล์

---
+++ config/middleware.js
@@ -0,0 +1,15 @@
+module.exports = {
+    //...
+    settings: {
+      cache: {
+        enabled: true,
+        models: [
+          "alphabet-index",
+          "blogs",
+          "family-index",
+          "setting-footer",
+          "youtube"
+        ],
+      },
+    },
+  };
