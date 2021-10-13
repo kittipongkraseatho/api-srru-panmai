6b2653faa26f558cd6f3685dd56e8ec68aa2221d
kittipongkraseatho <kittipong.kr.57@gmail.com>
2021-10-13 13:29:41 +0700

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
-module.exports = {
+ module.exports = {
     apps: [
       {
         name: 'api-panmai',
+        script: 'server.js',
+        args: 'start',
         exec_mode: 'cluster',
         instances: '1', // Or a number of instances
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start'
-      }
-    ]
-  }
+     
+      },
+    ],
+  };
