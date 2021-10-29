aa075e7a812ee5a8964777b104ea9ff888866b2c
kittipongkraseatho <kittipong.kr.57@gmail.com>
2021-10-29 10:59:49 +0700

    update

--- config/database.js
+++ config/database.js
@@ -3,13 +3,22 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'bookshelf',
+      // settings: {
+      //   client: 'mysql',
+      //   host: env('DATABASE_HOST', '103.13.229.230'),
+      //   port: env.int('DATABASE_PORT', 3306),
+      //   database: env('DATABASE_NAME', 'admin_panmai'),
+      //   username: env('DATABASE_USERNAME', 'admin'),
+      //   password: env('DATABASE_PASSWORD', 'wandeevps'),
+      //   ssl: env.bool('DATABASE_SSL', false),
+      // },
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', '103.13.229.230'),
+        host: env('DATABASE_HOST', '103.13.231.38'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'admin_panmai'),
-        username: env('DATABASE_USERNAME', 'admin'),
-        password: env('DATABASE_PASSWORD', 'wandeevps'),
+        database: env('DATABASE_NAME', 'arlor_panmai'),
+        username: env('DATABASE_USERNAME', 'arlor'),
+        password: env('DATABASE_PASSWORD', 'arlor'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
